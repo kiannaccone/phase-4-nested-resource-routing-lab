8b198bcb0dcf709dec214c20ca3ffa3dde3e49c2
Katherine Iannaccone <katherineiannaccone@yahoo.com>
2021-11-29 10:27:19 -0700

    commit tree 022f8873aa0c675a338ff3230eb7219268cc5315

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,34 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
+  end
+
+  def show
+    items = Item.find(params[:id])
+    render json: items
+  end
+
+  def create
+    items = Item.create!(items_params)
+    render json: items, status: :created
+  end
+
+  private
+
+  def render_not_found_response
+    render json: { error: "Not found" }, status: :not_found
+  end
+
+  def items_params
+    params.permit(:name, :description, :price, :user_id)
   end
 
 end
